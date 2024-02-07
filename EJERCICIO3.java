cf55d4bac9085351ca3ffc994af708f45090ef21
Sabil667 <msabi@myuax.com>
2024-02-07 14:29:24 +0100

    .

--- EJERCICIO3.java
+++ EJERCICIO3.java
@@ -3,3 +3,19 @@ public class EJERCICIO3 {
     public static void main(String[] args) {
         int begin = 1;
         int end = 100;
+
+        int suma = 0;
+        int cantidadMultiplos = 0;
+
+        for (int i = begin; i <= end; i++) {
+            if (i % 5 == 0) {
+                suma += i;
+                cantidadMultiplos++;
+                System.out.println("Múltiplo de 5: " + i);
+            }
+        }
+
+        System.out.println("La suma de los múltiplos de 5 es: " + suma);
+        System.out.println("Cantidad de múltiplos de 5: " + cantidadMultiplos);
+    }
+}
