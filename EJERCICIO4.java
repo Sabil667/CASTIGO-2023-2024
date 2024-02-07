316aafe84901eec959b4b7fd974532fb677d29b1
Sabil667 <msabi@myuax.com>
2024-02-07 14:32:57 +0100

    .

---
+++ EJERCICIO4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class EJERCICIO4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        int minimo = Integer.MAX_VALUE;
+        int maximo = Integer.MIN_VALUE;
+}
